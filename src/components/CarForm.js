1370a575b3593d74535b5f1391a0145f6b1220b1
Thiago <tlcrezende@gmail.com>
2023-02-13 14:28:14 -0300

    add dispacth, addcard, changename reducers

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -3,6 +3,8 @@ import { changeName, changeCost, addCar } from '../store';
 
 function CarForm() {
   const dispatch = useDispatch();
+
+  // useSelector é a função que faz essa parte do código ter acesso aos estados do reducer. Assim, conseguimos acessar o state do slice form e objeto name.
   const { name, cost } = useSelector((state) => {
     return {
       name: state.form.name,
@@ -10,16 +12,21 @@ function CarForm() {
     };
   });
 
+  // função responsável por mudar o valor do formulário quando o usuário digita, isto é, muda o state do nome cars a cada "teclada"
+  // dispath é a forma que o reducer faz para chamar a função changeName que muda o nome do carro
   const handleNameChange = (event) => {
     dispatch(changeName(event.target.value));
   };
+
+  //parseInt previni que apenas numeros possam ser escritos, ou um 0
   const handleCostChange = (event) => {
     const carCost = parseInt(event.target.value) || 0;
     dispatch(changeCost(carCost));
   };
   const handleSubmit = (event) => {
+    // tira o padrão normal do navegador de querer dar refresh na página toda vez que tem a entrada de um submit
     event.preventDefault();
-
+    // chama a função de adicionar caro, ou melhor, enviar name e cost para a função do reducer
     dispatch(addCar({ name, cost }));
   };
 
@@ -41,6 +48,7 @@ function CarForm() {
             <label className="label">Cost</label>
             <input
               className="input is-expanded"
+              // empty string é para "tirar" o 0 que fica lá insistente no formulário
               value={cost || ''}
               onChange={handleCostChange}
               type="number"
